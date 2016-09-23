7b91e7fe7b5e884c258ba1990a6f67274550e28d
Derek Liu <liu.derekt@gmail.com>
2016-09-23 16:41:58 -0700

    Add morgan middlware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,6 +1,8 @@
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 module.exports = function(app, express) {
+  app.use(morgan('combined'));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
 };
