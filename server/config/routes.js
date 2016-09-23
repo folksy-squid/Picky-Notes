9a724dfb3ba3349db6ae5c64e5453a630bf13a27
Derek Liu <liu.derekt@gmail.com>
2016-09-23 16:55:54 -0700

    Add endpoints for Facebook OAuth and User Creation

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,21 @@
 module.exports = function(app, express) {
 
-  app.get('/test', function(req, res) {
-    res.send('Testing123');
+  app.get('/auth/facebook', function(req, res) {
+    res.send('Facebook OAuth');
   });
 
+  app.get('auth/facebook/callback', function(req, res) {
+    res.send('Callback for Facebook OAuth');
+  });
+
+  app.route('/api/users')
+    .post(function(req, res) {
+      res.send('Create a new user');
+    })
+    .put(function(req, res) {
+      res.send('Update user info');
+    })
+    .delete(function(req, res) {
+      res.send('Delete the user');
+    });
 };
