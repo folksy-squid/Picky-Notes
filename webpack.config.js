7e6a0c1be71e57bde4ed52326018d66e209e3fbd
Sean <shonum@gmail.com>
2016-09-23 19:44:04 -0700

    change the webpack configurations to support react and es6

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,10 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel-loader'
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
     }]
   }
 };
