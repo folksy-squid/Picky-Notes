8c950698d67b93b0f4c5827502c335e055be374f
Derek Liu <liu.derekt@gmail.com>
2016-09-24 15:56:12 -0700

    (dev) turn off db logging

--- server/database/db-config.js
+++ server/database/db-config.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://ubuntu:password@localhost:5432/notepicker');
+const db = new Sequelize('postgres://ubuntu:password@localhost:5432/notepicker', { logging: false });
 
 const {User} = require('./UserController')(db, Sequelize);
 const {Room} = require('./RoomController')(db, Sequelize, User);
