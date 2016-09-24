f5712277dcd3e53fa1870f0d6190cad0f991469f
Derek Liu <liu.derekt@gmail.com>
2016-09-24 15:41:03 -0700

    Add basic ServerSpec test

---
+++ specs/Server/ServerSpec.js
@@ -0,0 +1,10 @@
+var request = require('supertest');
+var express = require('express');
+var expect = require('chai').expect;
+var app = require('../../server/server');
+
+describe('Endpoints', function() {
+  it('should pass this test', function() {
+    expect(true).to.equal(true);
+  });
+});
