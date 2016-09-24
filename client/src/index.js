692d17f5d3ac5cff1479ecd7971e334709baa6fb
Sean <shonum@gmail.com>
2016-09-23 19:50:04 -0700

    add redux content to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,14 @@
+/*jshint esversion: 6 */
+import reducers from './reducers';
+import App from './components/App.jsx';
 
-console.log('STORE', Redux.createStore);
+const store = Redux.createStore(reducers);
+const rootEl = document.getElementById('root');
+
+const render = () => ReactDOM.render(
+  <App />,
+  rootEl
+)
+
+render();
+store.subscribe(render);
