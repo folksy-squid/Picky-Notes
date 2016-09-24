abd1af9d782ced5e5a7c334a4e9af006aaac7fd7
k-rathi <kunalrathi1994@gmail.com>
2016-09-23 19:45:44 -0700

    changing db

---
+++ server/database/NoteController.js
@@ -0,0 +1,27 @@
+module.exports = (db, Sequelize, User) => {
+  var Note = db.define('note', {
+    content: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    audioTimestamp: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    show: {
+      type: Sequelize.BOOLEAN,
+      default: false
+    },
+    edittingUserId: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      references: {
+        model: User,
+        foreignKey: "facebookId"
+      }
+    }
+  });
+  return {
+    Note: Note
+  };
+};
