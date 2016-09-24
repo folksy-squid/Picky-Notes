38ed99078ce7ed579dd72eb1fd493ce756473b59
Derek Liu <liu.derekt@gmail.com>
2016-09-23 17:08:50 -0700

    Add endpoints for Room creation (start of lecture), Note Creation (end of lecture), Editing Notes

--- server/config/routes.js
+++ server/config/routes.js
@@ -19,4 +19,30 @@ module.exports = function(app, express) {
     .delete(function(req, res) {
       res.send('Delete the user');
     });
+
+  // Room Creation
+  app.post('/api/rooms', function(req, res) {
+    res.send('Create a room');
+  });
+
+  // Note Creation
+  app.post('/api/notes/new', function(req, res) {
+    res.send('End of lecture, and create all new notes for each user');
+  });
+
+  // Note Editing
+  app.route('/api/notes')
+    .get(function(req, res) {
+      /*
+       * /api/notes/?userId=id&roomId=id // compare all notes for user
+       * /api/notes/?userId=id&roomId=id&filter=show // get all notes for that lecture
+       */
+      res.send('Compare all notes for the user/room');
+    })
+    .put(function(req, res) {
+      res.send('Edit existing notes (save button)');
+    })
+    .post(function(req, res) {
+      res.send('Add new notes (save button)');
+    });
 };
