356d6062d99f509a495616510bbf7369b1086122
Sean <shonum@gmail.com>
2016-09-24 17:31:06 -0700

    Set up authentication and user methods to App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,59 @@
-export default class App extends React.Component () {
+export default class App extends React.Component {
+
   constructor(props) {
     super(props);
+    // REDUX?
+    this.state = {
+      isAuth: false,
+      user: {}
+    };
   }
-  render() {
+
+
+  componentWillMount() {   //  Retrieve the data, check if user logged in
+
+    var context = this;
+
+    $.get('/checkLogin').then(function(data) {
+      context.setState({
+        isAuth: data === 'authenticated'
+      });
+    }).catch(function(err) {
+      // REDUX?
+      context.setState({
+        isAuth: false
+      });
+    });
+
+    $.ajax({
+      method: 'GET',
+      url: 'api/user_data',
+      success: function(user) {
+        context.setUser(user);
+      }
+    });
+
+  }
+
+  checkAuthState () {
+    // REDUX?
+    return this.state.isAuth;
+  }
+
+  render() {   //  Depending on authentication, serves different nav-bar
+    var context = this;
+    var checkAuthState = this.checkAuthState;
+    var children = React.Children.map(this.props.children, function(child) {
+      return React.cloneElement(child, {
+        auth: checkAuthState,
+        user: context.state.user
+      });
+    });
     return (
-      <div>Hello World!</div>
+      <div>
+        <Navbar/>
+        {children}
+      </div>
     );
   }
 }
