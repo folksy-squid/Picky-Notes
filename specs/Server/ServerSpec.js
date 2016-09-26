6236af45270207385689880b21581df8c619e9e6
Derek Liu <liu.derekt@gmail.com>
2016-09-26 16:18:04 -0700

    (test) enable before function to create default user

--- specs/Server/ServerSpec.js
+++ specs/Server/ServerSpec.js
@@ -4,19 +4,18 @@ var expect = require('chai').expect;
 var app = require('../../server/server');
 var {db, User} = require('../../server/database/db-config');
 
-// before((done) => {
-//   db.sync({force: true})
-//   .then(() => {
-//     return User.create({
-//       facebookId: '10206128224638462',
-//       name: 'Kunal Rathi',
-//       email: 'volcanic.phoenix@gmail.com',
-//       pictureUrl: 'https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-1/p320x320/735019_3760102334957_1830986009_n.jpg?oh=95f952f6a491fa054cbb85122e45395f&oe=587471E6',
-//       gender: 'Male' 
-//     });
-//   })
-//   .then(() => done());
-// });
+before(() => {
+  db.sync()
+  .then(() => {
+    return User.findOrCreate({
+      facebookId: '10206128224638462',
+      name: 'Kunal Rathi',
+      email: 'volcanic.phoenix@gmail.com',
+      pictureUrl: 'https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-1/p320x320/735019_3760102334957_1830986009_n.jpg?oh=95f952f6a491fa054cbb85122e45395f&oe=587471E6',
+      gender: 'Male' 
+    });
+  });
+});
 
 describe('Room Creation', () => {
   it('should pass back a pathUrl to the room', (done) => {
