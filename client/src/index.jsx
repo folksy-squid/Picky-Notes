998bd3503268efe9d0a162be516fb551c149d8fb
Sean <shonum@gmail.com>
2016-09-25 17:27:25 -0700

    Import the new files and refactored so store files are in store.js instead

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,38 +1,46 @@
 /*jshint esversion: 6 */
 
-/* <- Imports and Declarations -> */
-import reducers from './reducers';
+/* <- Import components -> */
 import App from './components/App.jsx';
 import Landing from './components/Landing.jsx';
+import Main from './components/Main.jsx';
+import Lecture from './components/Lecture.jsx';
+import Compile from './components/Compile.jsx';
+import NewRoom from './components/NewRoom.jsx';
+import JoinRoom from './components/JoinRoom.jsx';
+import Lobby from './components/Lobby.jsx';
+import Notebook from './components/Notebook.jsx';
+import Review from './components/Review.jsx';
 
-// Declare Redux Methods
-const store = Redux.createStore(reducers);
+/* <- Import store -> */
+import store, {history} from './store';
 
-// Declare ReactRouter Methods
+/* <- Declarations -> */
 const Router = ReactRouter.Router;
 const Route = ReactRouter.Route;
 const IndexRoute = ReactRouter.IndexRoute;
-const browserHistory = ReactRouter.browserHistory;
-/*<-// Imports and Declarations -> */
+const Provider = ReactRedux.Provider;
 
+/* <- Set up router -> */
 const render = () => ReactDOM.render((
-  <Router history={browserHistory} >
-    <Route path='/' component={App} >
-      <IndexRoute component={Landing} />
-      <Route path='/main' component={Main}>
-        <Route path='/notebook' component={Notebook} />
-        <Route path='/new' component={NewRoom} />
-        <Route path='/join' component={JoinRoom} />
-        <Route path='/lobby' component={Lobby} />
-        <Route path='/review' component={Review} />
+  <Provider store={store}>
+    <Router history={history} >
+      <Route path='/' component={App} >
+        <IndexRoute component={Landing} />
+        <Route path='/main' component={Main}>
+          <Route path='/notebook' component={Notebook} />
+          <Route path='/new' component={NewRoom} />
+          <Route path='/join' component={JoinRoom} />
+          <Route path='/lobby' component={Lobby} />
+          <Route path='/review' component={Review} />
+        </Route>
+        <Route path='/lecture' component={Lecture} />
+        <Route path='/compile' component={Compile} />
       </Route>
-      <Route path='/lecture' component={Lecture} />
-      <Route path='/compile' component={Compile} />
-    </Route>
-  </Router>
+    </Router>
+  </Provider>
   ),
   document.getElementById('root')
 );
 
 render();
-store.subscribe(render);
