282872057083a8460a7cb4a722956529078e921b
Sean <shonum@gmail.com>
2016-09-26 16:35:50 -0700

    Connect notebooks to connection

--- client/src/components/Notebook.jsx
+++ client/src/components/Notebook.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import Connection from '../Connection.js'
 import { Link } from 'react-router';
 
 class Notebook extends React.Component {
   constructor (props) {
-    super(props)
+    super(props);
+    console.log('state:', this.state);
+    console.log('props:', props);
   }
-  render(){
 
+  render(){
+    return (
+      <div>'this is the notebook page'</div>
+    )
   }
 }
 
-export default Notebook;
+export default Connection(Notebook);
