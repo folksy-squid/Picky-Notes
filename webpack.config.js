b5ed511d43cdcc14020f67c163cc807742b4ad14
Sean <shonum@gmail.com>
2016-09-26 12:27:00 -0700

    (Fix) took out hotreload plugin and modules

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,11 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: [
-    'webpack-hot-middleware/client',
-    './client/src/index.jsx'
-  ],
+  entry: './client/src/index.jsx',
   output: {
     path: path.join(__dirname, '/client/dist'),
     filename: 'index.bundle.js',
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
