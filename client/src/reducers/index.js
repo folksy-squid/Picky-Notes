59e9b529f24fca2c89fb8501bf4f52cacc00895e
Sean <shonum@gmail.com>
2016-09-25 17:28:24 -0700

    Add routerReducer to combineReducer under routing key

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,12 @@
 /* jshint esversion: 6 */
+const combineReducers = Redux.combineReducers;
+const routerReducer = ReactRouterRedux.routerReducer;
+
 import reducer1 from './reducer1.js';
 import reducer2 from './reducer2.js';
 
-export default Redux.combineReducers({ reducer1, reducer2 });
+export default combineReducers({
+  reducer1,
+  reducer2,
+  routing: routerReducer
+});
