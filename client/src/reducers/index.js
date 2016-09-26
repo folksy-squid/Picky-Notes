3b651706bcb68955e5e6e78320257a47f0f9f52a
Marco Chan <inspiredtolive0@gmail.com>
2016-09-25 17:57:50 -0700

    Remove unnecessary extension

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,5 @@
 /* jshint esversion: 6 */
-import reducer1 from './reducer1.js';
-import reducer2 from './reducer2.js';
+import reducer1 from './reducer1';
+import reducer2 from './reducer2';
 
 export default Redux.combineReducers({ reducer1, reducer2 });
