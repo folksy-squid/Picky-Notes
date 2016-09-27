0c93889fb93a675d0fbd979ba2767c703627e50e
Sean <shonum@gmail.com>
2016-09-27 15:31:04 -0700

    Set up components from lecture view

--- client/src/components/sub/LectureTitle.jsx
+++ client/src/components/sub/LectureTitle.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Connection from '../Connection.js'
+import Connection from '../../Connection.js';
 
 const LectureTitle = (props) => (
   <h3 className="lectureTitle">Lecture Title by Professor Name</h3>
-)
+);
 
 export default Connection(LectureTitle);
