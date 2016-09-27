1b414a6e85fea19a0a190770c4fe6b95a20b7598
Sean <shonum@gmail.com>
2016-09-27 11:53:28 -0700

    Change class to className

--- client/src/components/sub/SearchBar.jsx
+++ client/src/components/sub/SearchBar.jsx
@@ -3,11 +3,11 @@ import Connection from '../Connection.js'
 import { Link } from 'react-router';
 
 const SearchBar = (props) => (
-  <form class="navbar-form" role="search">
-    <div class="form-group">
-      <input type="text" class="form-control" placeholder="Search">
+  <form className="navbar-form" role="search">
+    <div className="form-group">
+      <input type="text" className="form-control" placeholder="Search">
     </div>
-    <button type="submit" class="btn btn-default">Go</button>
+    <button type="submit" className="btn btn-default">Go</button>
   </form>
 )
 
