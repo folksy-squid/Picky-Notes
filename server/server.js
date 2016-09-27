10a4c0fedefaf1aff610fc1cb16349ce8e1544b1
k-rathi <kunalrathi1994@gmail.com>
2016-09-27 10:40:58 -0700

    had to change server to not run twice on tests

--- server/server.js
+++ server/server.js
@@ -12,21 +12,23 @@ require('./config/routes.js')(app, express);
 // set port depending on prod or dev
 const port = process.env.NODE_ENV === 'production' ? 80 : 3000;
 
-const listen = app.listen(port, () => {
-  console.log('Server listening on port ' + port);
-  db.sync();                  // use for production
-  // db.sync({force: true});     // use for development when you need to drop database
-  // .then(() => {
-  //   console.log('Database is synced');
-  // });
-});
+if (!module.parent) {
+  const listen = app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+    db.sync();                  // use for production
+    // db.sync({force: true});     // use for development when you need to drop database
+    // .then(() => {
+    //   console.log('Database is synced');
+    // });
+  });
 
-var io = require('socket.io').listen(listen);
+  var io = require('socket.io').listen(listen);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => console.log('a user disconnected'));
-});
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => console.log('a user disconnected'));
+  });
+}
 
 
 module.exports = app;
