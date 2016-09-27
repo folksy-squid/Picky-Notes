202843e19725de5c1b4632cc574e027883b5c446
Sean <shonum@gmail.com>
2016-09-27 12:11:24 -0700

    Add closing tag to inputs in jsx

--- client/src/components/NewRoom.jsx
+++ client/src/components/NewRoom.jsx
@@ -12,9 +12,9 @@ class NewRoom extends React.Component {
         <h2>New Room</h2>
         <div>
           <form>
-            <input type="text" name="subject" placeholder="Subject">
-            <input type="text" name="class" placeholder="Class">
-            <input type="text" name="lecturer" placeholder ="Lecturer (optional)">
+            <input type="text" name="subject" placeholder="Subject" />
+            <input type="text" name="class" placeholder="Class" />
+            <input type="text" name="lecturer" placeholder ="Lecturer (optional)" />
           </form>
         </div>
       </div>
