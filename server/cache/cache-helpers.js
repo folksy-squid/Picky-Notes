d18ad839f09f6d37a1f46a8e4c4bbccfb5d1c067
Derek Liu <liu.derekt@gmail.com>
2016-09-27 19:36:57 -0700

    (dev) remove pub from redis test functions

--- server/cache/cache-helpers.js
+++ server/cache/cache-helpers.js
@@ -1,4 +1,4 @@
-const {cache, pub} = require('./cache-config');
+const {cache} = require('./cache-config');
 
 const test = () => {
   cache.set('newNote', 'a new thing')
@@ -6,21 +6,5 @@ const test = () => {
   .then((data) => {
     console.log(data);
     cache.quit();
-  });
-};
-
-const pubsubTest = () => {
-  cache.subscribe('news', 'music', function (err, count) {
-    // Now we are subscribed to both the 'news' and 'music' channels.
-    // `count` represents the number of channels we are currently subscribed to.
-
-    pub.publish('news', 'Hello world!');
-    pub.publish('music', 'Hello again!');
-  });
-
-  cache.on('message', function (channel, message) {
-    // Receive message Hello world! from channel news
-    // Receive message Hello again! from channel music
-    console.log('Receive message %s from channel %s', message, channel);
   });
 };
