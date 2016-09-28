244d423ae9dda7ee84d2b1c50d7b300f6f3e2a47
Sean <shonum@gmail.com>
2016-09-28 16:57:07 -0700

    Add logout functionality

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -9,11 +9,10 @@ export default (state = {}, action) => {
 
   if (action.type === 'DELETE_USER'){
     if (window.document.cookie){
-      document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+      window.document.cookie += '; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+      delete state.information;
     }
-    state = {};
   }
-
 
   return state;
 };
