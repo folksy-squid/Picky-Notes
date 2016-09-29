070d59e1a4bf81ea2dead40bc35e3b424b007506
Sean <shonum@gmail.com>
2016-09-29 16:15:22 -0700

    Add coloring to icon depending on participant number

--- client/src/components/sub/ParticipantList.jsx
+++ client/src/components/sub/ParticipantList.jsx
@@ -6,31 +6,37 @@ class ParticipantList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      participants: [{name: this.props.getState().user.information[0].name}]
+      participants: [this.props.getState().user.information[0]]
     };
   }
 
   componentWillMount() {
-    console.log('this.props', this.props.getState())
+    console.log('this.props', this.props);
+    console.log('this.props state', this.props.getState());
     var socket = this.props.getState().room.socket;
+    // var participants = this.props.getState().room.participants;
+    // this.setState({
+    //   participants: participants
+    // })
 
-    // run action that will trigger reducer to get other users in the room
-    this.props.dispatch(getParticipants())
-
-
-    socket.on('new user joined room', (username) => {
-      this.setState({
-        participants: this.state.participants.concat([{name: username}])
-      });
-    })
-  };
+    // socket.on('new user joined room', (user) => {
+    //   this.setState({
+    //     participants: this.state.participants.concat([user])
+    //   });
+    // });
+  }
 
   render() {
+    var participantLength = this.state.participants.length;
+    var classColor = (i) => 'participant'+i;
     return (
       <div className="participants">
+        <h4>
+          Participants ({participantLength}/10)
+        </h4>
         {this.state.participants.map(({name}, i)=>
         <div key={i}>
-          <i className="ion ion-android-person" aria-hidden="true"></i>
+          <i className="ion ion-android-person {classColor(i)}" aria-hidden="true"></i>
           <span>{name}</span>
         </div>
       )}
