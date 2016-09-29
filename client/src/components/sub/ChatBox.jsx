f4931d903f5188104d75f2398789c05be6c54899
Sean <shonum@gmail.com>
2016-09-29 16:02:31 -0700

    Add chatbox

---
+++ client/src/components/sub/ChatBox.jsx
@@ -0,0 +1,13 @@
+export default class ChatBox extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    <div className="chatbox">
+      <h4>
+        Chattt here!!
+      </h4>
+    </div>
+  }
+}
