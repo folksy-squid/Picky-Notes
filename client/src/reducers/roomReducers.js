bef705938da81d46f423b86bf36139b955322c1a
Sean <shonum@gmail.com>
2016-09-28 19:12:39 -0700

    Create Socket Room set up

--- client/src/reducers/roomReducers.js
+++ client/src/reducers/roomReducers.js
@@ -11,15 +11,28 @@ export default (state = {}, action) => {
       data: action.data,
       success: function(res, status){
         console.log('the response: ', res);
-        state.roomInfo = res
-        // create a socket connection that uses a separate reducer
-        // because we need to make sure the same socket exists across all
+        state.socket = createSocketRoom(action.data.hostId, res.pathUrl);
+        state.roomInfo = res;
       },
       error: function( res, status ){
         console.log(res);
       }
     });
     console.log('data:', action.data);
+  }
+
+  var createSocketRoom = (hostId, pathUrl) => {
+    var socket = io();
+    socket.emit('create room', pathUrl, hostId);
+    return socket;
+  };
+
+  if (action.type === 'JOIN_SOCKET_ROOM'){
+
+  }
+
+  if (action.type === 'DELETE_SOCKET_ROOM'){
+
   }
 
 
