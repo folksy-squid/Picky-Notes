fd9fe2f725f8e87614d7c7a7f852a3a7b150b0c2
Sean <shonum@gmail.com>
2016-09-28 21:15:05 -0700

    Add join room and create room to room reducers

--- client/src/reducers/roomReducers.js
+++ client/src/reducers/roomReducers.js
@@ -1,19 +1,35 @@
 /*jshint esversion: 6 */
+const createSocketRoom = (hostId, pathUrl, cb) => {
+  // this is if server is localhost
+  var socket = io();
+  // if server is not localhost,
+  // then set up io to know where the server is
+  socket.emit('create room', pathUrl, hostId);
+  socket.on('create room success', function(){
+    console.log('successfully created socket room');
+    cb(null, pathUrl);
+    return socket;
+  });
+};
+
 
 export default (state = {}, action) => {
-
-  if (action.type === 'CREATE_ROOM'){
+  if (action.type === 'CREATE_ROOM') {
     // ajax call passing in action.data and then setting state in the success
     $.ajax({
       method: 'POST',
       url: '/api/rooms',
-      data: action.data,
+      contentType: 'application/json',
+      data: JSON.stringify(action.data),
       success: function(res, status){
         console.log('the response: ', res);
-        state.socket = createSocketRoom(action.data.hostId, res.pathUrl);
-        state.roomInfo = res;
+        return {
+          ...state,
+          socket: createSocketRoom(action.data.hostId, res.pathUrl, action.cb),
+          roomInfo: res
+        }
       },
-      error: function( res, status ){
+      error: function( res, status ) {
         console.log(res);
       }
     });
@@ -24,12 +40,14 @@ export default (state = {}, action) => {
     socket.emit('join room', action.pathUrl, action.userId);
 
     socket.on('join room error', () => {
+      console.log('we have failed to join a room');
       socket.disconnect();
       state.socket = null;
       action.cb('error');
     });
 
     socket.on('join room success', () => {
+      console.log('we have successfully joined a room', socket);
       state.socket = socket;
       action.cb(null, 'success');
     });
@@ -42,16 +60,4 @@ export default (state = {}, action) => {
 
 
   return state;
-};
-
-const createSocketRoom = (hostId, pathUrl) => {
-  // this is if server is localhost
-  var socket = io();
-  // if server is not localhost,
-  // then set up io to know where the server is
-  socket.emit('create room', pathUrl, hostId);
-  socket.on('create room success', function(){
-    console.log('successfully created socket room');
-    return socket;
-  });
 };
