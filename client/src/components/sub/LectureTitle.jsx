62f684f7d7ffda1e1093579bfa5f194aa5a215f0
Sean <shonum@gmail.com>
2016-09-29 17:26:09 -0700

    Lobby component changes

--- client/src/components/sub/LectureTitle.jsx
+++ client/src/components/sub/LectureTitle.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import Connection from '../../Connection';
 const LectureTitle = (props) => {
+
   var roomInfo = props.getState().room.roomInfo;
+
   var lecturer = roomInfo.lecturer;
   var topic = roomInfo.topic;
-  var createdAt = moment(roomInfo.createdAt)
+  var createdAt = moment(roomInfo.createdAt);
 
   return (
     <h3 className="lectureTitle">{topic} by {lecturer}</h3>
-  )
+  );
 };
 
-export default LectureTitle;
+export default Connection(LectureTitle);
