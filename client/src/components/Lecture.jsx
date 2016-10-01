cb669c29a99eec5813c4f5caf27e4d1e937b95b4
Derek Liu <liu.derekt@gmail.com>
2016-09-30 18:19:26 -0700

    create continue button to compile view  when all users are ready

--- client/src/components/Lecture.jsx
+++ client/src/components/Lecture.jsx
@@ -8,14 +8,36 @@ import ParticipantList from './sub/ParticipantList.jsx';
 class Lecture extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      continueButtonDisplay: 'none',
+      readyButtonDisplay: 'inline-block'
+    };
+  }
+
+  componentWillMount() {
+    this.props.getState().room.socket.on('all ready', () => {
+      // loading
+      console.log('user is ready');
+    });
+
+    this.props.getState().room.socket.on('all notes saved', () => {
+      // redirect to compile view
+      console.log('all notes saved and redirect');
+      this.setState({continueButtonDisplay: 'inline-block'});
+    });
   }
 
   sendReady() {
     this.props.getState().room.socket.emit('user ready');
+    this.setState({readyButtonDisplay: 'none'});
   }
 
   endLecture() {
     this.props.getState().room.socket.emit('lecture end');
+  }
+
+  redirectToCompile () {
+
   }
 
   render() {
@@ -31,9 +53,12 @@ class Lecture extends React.Component {
           <Link className="btn btn-lg btn-danger" to="/compile">
             End Lecture
           </Link>
-          <button className="btn btn-lg btn-success" onClick={this.sendReady.bind(this)}>
+          <button className="btn btn-lg btn-success" style={{display: this.state.readyButtonDisplay}} onClick={this.sendReady.bind(this)}>
             Ready
           </button>
+          <Link className="btn btn-lg btn-success" style={{ display: this.state.continueButtonDisplay}} to="/compile">
+            Continue
+          </Link>
         </div>
         <div className="row">
           <div className="col-md-9">
