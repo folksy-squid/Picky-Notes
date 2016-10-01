a8c24c24e3060c4bb4b72ab61f1a8a4214b06093
Sean <shonum@gmail.com>
2016-10-01 12:55:41 -0700

    fthis

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,52 @@
 var path = require('path');
-
+var webpack = require('webpack');
 
 // Add spec files to compile
 module.exports = {
-  entry: './client/src/index.jsx',
+  entry: [
+    //entry for webpack to look for current code when change
+    'webpack-hot-middleware/client',
+    //entry to create bundle for the first time entering app
+    './client/src/index.jsx'
+  ],
   output: {
-    path: path.join(__dirname, '/client/dist'),
+    // path: path.join(__dirname, '/client/dist'),
+    path: path.resolve('./client/dist'),
     filename: 'index.bundle.js',
-    publicPath: 'http://localhost:7700/dist',
+    publicPath: '/'
   },
-  devServer: {
-    hot: true,
-    inline: true,
-    port: 7700,
-    historyApiFallback: true
-  },
+  // devServer: {
+  //   hot: true,
+  //   inline: true,
+  //   port: 7700,
+  //   historyApiFallback: true
+  // },
   externals: {
     'cheerio': 'window',
     'react/addons': true,
     'react/lib/ExecutionEnvironment': true,
     'react/lib/ReactContext': true
   },
+  plugins: [
+    // optimizes the order and efficiency of module usage
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    // allows changes to be replaced 'hot-modularly'. >> A middleware within your server
+    new webpack.HotModuleReplacementPlugin(),
+    // won't allow webpack to finish the build if it encounters an error
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['react', 'es2015']
+        presets: ['react', 'es2015', 'react-hmre']
       }
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
     }]
   },
-  devtool: 'source-map'
+  devtool: 'inline-source-map'
 };
