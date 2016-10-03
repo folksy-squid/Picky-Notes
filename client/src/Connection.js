3c728efb78995f2f13f437aea53200a2ab5516d8
Sean <shonum@gmail.com>
2016-10-03 12:35:31 -0700

    (fix) state

--- client/src/Connection.js
+++ client/src/Connection.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import * as actions from './actions/index.js';
 import store from './store';
 
-const mapStateToProps = () => (store);
+const mapStateToProps = (state) => (store);
 
 
 const mapDispatchToProps = (dispatch) =>
