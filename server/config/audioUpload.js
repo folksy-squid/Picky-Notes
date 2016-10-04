e4fdd115ec091e85633c1892f85ef52ce9375e6d
k-rathi <kunalrathi1994@gmail.com>
2016-10-04 16:48:22 -0700

    remove keys from testing

--- server/config/audioUpload.js
+++ server/config/audioUpload.js
@@ -3,9 +3,15 @@ const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
-const accessKeyId = require('../../keys.js').aws.AWS_ACCESS_KEY_ID;
-const secretAccessKey = require('../../keys.js').aws.AWS_SECRET_ACCESS_KEY;
-const region = require('../../keys.js').aws.AWS_REGION;
+if (process.env.NODE_ENV==='test') {
+  const accessKeyId = require('../../example_keys.js').aws.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = require('../../example_keys.js').aws.AWS_SECRET_ACCESS_KEY;
+  const region = require('../../example_keys.js').aws.AWS_REGION;
+} else {
+  const accessKeyId = require('../../keys.js').aws.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = require('../../keys.js').aws.AWS_SECRET_ACCESS_KEY;
+  const region = require('../../keys.js').aws.AWS_REGION;
+}
 
 const s3 = new AWS.S3({
   accessKeyId: accessKeyId,
