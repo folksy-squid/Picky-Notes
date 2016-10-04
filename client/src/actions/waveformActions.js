ec8e3740fa9b13945d3e61e9b5d44d45ef4ec258
k-rathi <kunalrathi1994@gmail.com>
2016-10-04 15:14:37 -0700

    added waveform actions

---
+++ client/src/actions/waveformActions.js
@@ -0,0 +1,29 @@
+export const togglePlay = () => {
+  return {
+    type: 'TOGGLE_PLAY'
+  };
+};
+export const setPos = (pos) => {
+  return {
+    type: 'SET_POS',
+    pos
+  };
+};
+export const setVolume = (volume) => {
+  return {
+    type: 'SET_VOLUME',
+    volume
+  };
+};
+export const setAudioRateChange = (audioRate) => {
+  return {
+    type: 'SET_AUDIO_RATE_CHANGE',
+    audioRate
+  };
+};
+
+export const play = () => {
+  return {
+    type: 'START_PLAY'
+  };
+};
