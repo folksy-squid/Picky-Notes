538275cfc1bc1bc8e204c201b5d64dd096522901
k-rathi <kunalrathi1994@gmail.com>
2016-10-04 20:03:16 -0700

    fixed the lecture title comment removal

--- client/src/components/sub/LectureTitle.jsx
+++ client/src/components/sub/LectureTitle.jsx
@@ -3,20 +3,13 @@ import {connect} from 'react-redux';
 import {getCurrentView} from '../../helpers.js';
 import RoomReducer from '../../reducers/roomReducers';
 class LectureTitle extends React.Component {
-  /*----------  NPM RUN DEV  ----------*/
   constructor(props) {
     super(props);
     this.state = {
       createdAt: moment(props.room.roomInfo.createdAt).format('MM/DD/YYYY')
     };
   }
-  /*----------  //NPM RUN DEV  ----------*/
 
-  /*----------  NPM RUN CLIENT  ----------*/
-  // var lecturer = 'Mr Potatohead'
-  // var topic = 'World History';
-  // var createdAt = ... ;
-  /*----------  // NPM RUN CLIENT  ----------*/
   render() {
     var view = (
       <h3 className="lectureTitle">{this.props.room.roomInfo.topic} by {this.props.room.roomInfo.lecturer}</h3>
