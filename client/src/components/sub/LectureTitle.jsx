be98543a645d9ac41cd2e5f25e5326b7ee2ccd25
k-rathi <kunalrathi1994@gmail.com>
2016-10-04 19:14:25 -0700

    refactored to work with store

--- client/src/components/sub/LectureTitle.jsx
+++ client/src/components/sub/LectureTitle.jsx
@@ -2,32 +2,43 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {getCurrentView} from '../../helpers.js';
 import RoomReducer from '../../reducers/roomReducers';
-const LectureTitle = (props) => {
-  var roomInfo = props.room.roomInfo;
-  var lecturer = roomInfo.lecturer;
-  var topic = roomInfo.topic;
-  var createdAt = moment(roomInfo.createdAt).format('MM/DD/YYYY');
-
-  var view = (
-    <h3 className="lectureTitle">{topic} by {lecturer}</h3>
-  );
-
-  var pathname = props.routing.locationBeforeTransitions.pathname;
-
-  if (getCurrentView(pathname) === 'review') {
-    view = (
-    <div className="review-header">
-      <h4 className="lectureTitle">
-        {topic} by {lecturer}
-      </h4>
-      <h4 className="review-date">
-        {createdAt}
-      </h4>
-    </div>
-    );
+class LectureTitle extends React.Component {
+  /*----------  NPM RUN DEV  ----------*/
+  constructor(props) {
+    super(props);
+    this.state = {
+      createdAt: moment(props.room.roomInfo.createdAt).format('MM/DD/YYYY')
+    };
   }
-  return view;
-};
+  /*----------  //NPM RUN DEV  ----------*/
+
+  /*----------  NPM RUN CLIENT  ----------*/
+  // var lecturer = 'Mr Potatohead'
+  // var topic = 'World History';
+  // var createdAt = ... ;
+  /*----------  // NPM RUN CLIENT  ----------*/
+  render() {
+    var view = (
+      <h3 className="lectureTitle">{this.props.room.roomInfo.topic} by {this.props.room.roomInfo.lecturer}</h3>
+    );
+
+    var pathname = this.props.routing.locationBeforeTransitions.pathname;
+
+    if (getCurrentView(pathname) === 'review') {
+      view = (
+        <div className="review-header">
+        <h4 className="lectureTitle">
+        {this.props.room.roomInfo.topic} by {this.props.room.roomInfo.lecturer}
+        </h4>
+        <h4 className="review-date">
+        {this.state.createdAt}
+        </h4>
+        </div>
+      );
+    }
+    return view;
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
