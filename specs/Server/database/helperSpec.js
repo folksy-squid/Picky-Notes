fe599322ac970c089ce7e8e202eb31f4438769de
Derek Liu <liu.derekt@gmail.com>
2016-10-06 11:34:10 -0700

    (fix) modify example data for tests

--- specs/Server/database/helperSpec.js
+++ specs/Server/database/helperSpec.js
@@ -1,11 +1,22 @@
 const helpers = require('../../../server/database/db-helpers');
 const expect = require('chai').expect;
 describe('multiply notes', () => {
-  const notes = [
-    {originalUserId: 0, editingUserId: 0},
-    {originalUserId: 1, editingUserId: 1},
-    {originalUserId: 2, editingUserId: 2}
-  ];
+  const notes = [{
+    content: 'This is a note for User 0',
+    show: true,
+    originalUserId: 0,
+    editingUserId: 0
+  }, {
+    content: 'This is a note for User 1',
+    show: true,
+    originalUserId: 1,
+    editingUserId: 1
+  }, {
+    content: 'This is a note for User 2',
+    show: true,
+    originalUserId: 2,
+    editingUserId: 2
+  }];
 
   const clients = ['0', '1', '2'];
 
