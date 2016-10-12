8f0f1e655626f0fdb7c56b6a52b073ff045c7cdc
Sean <shonum@gmail.com>
2016-10-12 15:16:08 -0700

    (clean) remove useless comments from index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,37 +15,6 @@ import {getCurrentView, getRoomCodeFromUrl} from './helpers';
 
 const routes = createRoutes(store);
 
-// const checkLobby = ({roomCode}) => {
-
-//   let user = store.getState().user.information && store.getState().user.information[0];
-//   let participants = store.getState().room.participants;
-//   let host = participants && participants[0];
-
-//   if (host && user && host.id !== user.id) {
-//     store.dispatch(joinSocketRoom(roomCode, user, (err, success)=>{
-//       if (err){
-//         console.log('there was an error joining the room.');
-//       } else {
-//         console.log('join room success.');
-//       }
-//     }))
-//   } else {
-//     // redirect to 404 page...
-//     console.log('sorry not allowed....')
-//   }
-// }
-
-// // if you need to perform checks before mounting component
-// const getInfo = {
-//   'lobby': checkLobby
-// };
-
-// history.listen(location=> {
-//   let currentView = getCurrentView(location.pathname);
-//   let roomCode = getRoomCodeFromUrl(location.pathname);
-//   return getInfo[currentView] && getInfo[currentView]({roomCode});
-// });
-
 render((<Provider store={store}>
     <Router history={history} >
       {routes}
