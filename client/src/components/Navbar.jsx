e6b340f8b7eb769f9c3a105d6b54a890c4594ba4
Sean <shonum@gmail.com>
2016-10-11 21:19:46 -0700

    SO done with the joinroom modal

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -64,6 +64,18 @@ class Navbar extends React.Component {
     =            Modal            =
     =============================*/
 
+    // <span class="input input--nao">
+    //   <input class="input__field input__field--nao" type="text" id="input-1" />
+    //   <label class="input__label input__label--nao" for="input-1">
+    //     <span class="input__label-content input__label-content--nao">Username</span>
+    //   </label>
+    //   <svg class="graphic graphic--nao" width="300%" height="100%" viewBox="0 0 1200 60" preserveAspectRatio="none">
+    //     <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"/>
+    //   </svg>
+    // </span>
+
+    // <input ref="joinRoomInput" onChange={this.updateInput.bind(this)} type="text" className="form-control" placeholder="Enter Access Code" />
+
     let modal = (
       <Modal show={this.state.show} onHide={close}>
         <Modal.Header closeButton>
@@ -72,8 +84,17 @@ class Navbar extends React.Component {
         <Modal.Body>
           <div>
           <form onSubmit={this.submitInput.bind(this)} className="" role="join">
-            <div className="form-group">
-              <input ref="joinRoomInput" onChange={this.updateInput.bind(this)} type="text" className="form-control" placeholder="Enter Access Code" />
+            <div className="form-group form__nao">
+              <span className="input input--nao">
+                <input className="input__field input__field--nao" type="text" id="input-1" onChange={this.updateInput.bind(this)}/>
+                <label className="input__label input__label--nao" for="input-1">
+                  <span className="input__label-content input__label-content--nao">Enter Access Code</span>
+                </label>
+                <svg className="graphic graphic--nao" width="300%" height="100%" viewBox="0 0 1200 60" preserveAspectRatio="none">
+                  <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"/>
+                </svg>
+              </span>
+
             {(this.state.error) ?
               (<div className="alert alert-danger">
                 <a data-dismiss="alert">&times;</a>
