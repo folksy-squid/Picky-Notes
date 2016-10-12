7f42fb4578b7d0de3daf98cb1b43a48ee8a5505e
Sean <shonum@gmail.com>
2016-10-11 22:35:50 -0700

    (style) the notebook page and replace grid with sticky notes

--- client/src/components/sub/Entry.jsx
+++ client/src/components/sub/Entry.jsx
@@ -34,12 +34,26 @@ export default class Entry extends React.Component {
     // Render an Entry component for each entry
     // (use map)
     return (
-      <div className="col-sm-6 col-md-4 notebook-entry" onClick={this.clickHandler.bind(this)}>
-        <i className={`fa fa-sticky-note-o fa-5x notebook${this.props.classColor} notebook-icon`} aria-hidden="true"></i>
-        <div className='notebook-topic'>{`${this.props.entry.topic}`}</div>
-        <div className='notebook-lecturer'>{`by ${this.props.entry.lecturer}`}</div>
-        <div className='notebook-class'>{`${this.props.entry.class}`}</div>
+      <div className="col-xs-6 col-sm-4 col-md-3 notebook-entry" onClick={this.clickHandler.bind(this)}>
+        <div className={`w3-card-2 notebook${this.props.classColor}`}>
+          <div className="w3-container">
+            <br />
+            <div className='notebook-topic'>{`${this.props.entry.topic}`}</div>
+            <div className='notebook-lecturer'>{`by ${this.props.entry.lecturer}`}</div>
+            <div className='notebook-class'>{`${this.props.entry.class}`}</div>
+            <br />
+          </div>
+        </div>
       </div>
-    );
+    )
+
+    // return (
+    //   <div className="col-sm-6 col-md-4 notebook-entry" onClick={this.clickHandler.bind(this)}>
+    //     <i className={`fa fa-sticky-note-o fa-5x notebook${this.props.classColor} notebook-icon`} aria-hidden="true"></i>
+    //     <div className='notebook-topic'>{`${this.props.entry.topic}`}</div>
+    //     <div className='notebook-lecturer'>{`by ${this.props.entry.lecturer}`}</div>
+    //     <div className='notebook-class'>{`${this.props.entry.class}`}</div>
+    //   </div>
+    // );
   }
 }
