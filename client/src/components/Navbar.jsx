808be2f1fabc8da0c5a308303901da338a9e80ea
Sean <shonum@gmail.com>
2016-10-11 18:24:53 -0700

    Fix navbar with react-bootstrap and react-router-bootstrap

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Connection from '../Connection.js';
 import { Link } from 'react-router';
+import {IndexLinkContainer, LinkContainer} from 'react-router-bootstrap';
+import {Navbar as Navigation} from 'react-bootstrap';
+import {Nav, NavItem} from 'react-bootstrap';
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -21,36 +24,37 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <nav className="navbar navbar-default navbar-static-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed btn-info" data-toggle="collapse" data-target="#navbar-collapse1" onClick={toggleNav}>
-              <span className="sr-only">Toggle Navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#">Picky Notes</a>
-          </div>
-          <div className="navbar-collapse collapse" id="navbar-collapse1">
-            <ul className="nav navbar-nav navbar-right">
-              <li className="nav-item">
-                <Link className="nav-link " to="/notebook">My Lectures</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/join">Join Room</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/new">New Room</Link>
-              </li>
-              <li className="nav-item">
-                <a href="/" className="nav-link" onClick={this.logout.bind(this)} >Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
+      <Navigation>
+        <Navigation.Header>
+          <Navigation.Brand>
+            <a href="#">Picky Notes</a>
+          </Navigation.Brand>
+          <Navigation.Toggle />
+        </Navigation.Header>
+        <Navigation.Collapse>
+          <Nav pullRight>
+            <IndexLinkContainer to="/">
+              <NavItem eventKey={1}>
+                My Lectures
+              </NavItem>
+            </IndexLinkContainer>
+            <LinkContainer to="/join">
+              <NavItem eventKey={2}>
+                Join Room
+              </NavItem>
+            </LinkContainer>
+            <LinkContainer to="/new">
+              <NavItem eventKey={3}>
+                New Room
+              </NavItem>
+            </LinkContainer>
+            <NavItem eventKey={4} href="/" onClick={this.logout.bind(this)}>
+              Logout
+            </NavItem>
+          </Nav>
+        </Navigation.Collapse>
+      </Navigation>
+    )
   }
 }
 
