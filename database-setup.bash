5bbad1e66e94cccd3870142f00a76f097559d462
k-rathi <kunalrathi1994@gmail.com>
2016-10-13 10:55:03 -0700

    travis

--- database-setup.bash
+++ database-setup.bash
@@ -4,7 +4,6 @@
 psql -d postgres -f ./postgres-setup.sql --echo-all
 
 export NODE_ENV='test'
-npm install -g node-gyp
-npm install
-cd node_modules/lame && npm install
+npm install -g yarn
+yarn
 exit 0
