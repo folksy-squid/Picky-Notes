1317b10050f780923da1c716561023fd74c07afa
k-rathi <kunalrathi1994@gmail.com>
2016-10-12 21:38:04 -0700

    thoughts expandable panel

--- client/src/components/sub/ThoughtList.jsx
+++ client/src/components/sub/ThoughtList.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import NoteReducer from '../../reducers/noteReducers';
 import RoomReducer from '../../reducers/roomReducers';
 import {connect} from 'react-redux';
-
+import ReactMarkdown from 'react-markdown';
+import Infinite from 'react-infinite';
 const ThoughtList = (props) => {
   let thoughtView = props.note.justThoughts.map((thought, i)=>(
     <div key={i} className="review thought">
-      {thought.content}
+      <ReactMarkdown source={thought.content} />
     </div>
   ));
 
   return props.note.showThoughts ? (
     <div className="thought-list">
-      {thoughtView}
+      <Infinite containerHeight={window.innerHeight - 300} elementHeight={(window.innerHeight - 300) / (props.note.justThoughts.length-1)}>
+        {thoughtView}
+      </Infinite>
     </div>
   ) : (<div></div>);
 };
